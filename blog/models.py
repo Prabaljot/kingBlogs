6bb7b83910e131437a61844781310d9a74f13458
Prabaljot <jotprabal5@gmail.com>
2023-06-12 16:59:37 +0530

    commit while backup

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Blogpost(models.Model):
+    catChoices = (
+        ('1','General'),
+        ('2','Technology'),
+        ('3','Food'),
+        ('4','Education and Facts'),
+        ('5','GK'),
+        ('6','News'),
+        ('7','Gaming'),
+        ('8','Entertainment and Movies'),
+        ('9','Sports'),
+        ('10','Lifestyle'),
+        ('11','Travel'),
+        ('12','Art and Design'),
+        ('13','Music'),
+        ('14','Finance'),
+        ('15','Photography'),
+        ('16','Health and Fitness'),
+    )
     post_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     cintrohead = models.CharField(max_length=5000, default="")
@@ -11,6 +29,7 @@ class Blogpost(models.Model):
     chead2 = models.CharField(max_length=5000, default="")
     con = models.CharField(max_length=500, default="")
     ccon = models.CharField(max_length=5000, default="")
+    category =  models.CharField(max_length=100, default="1", choices=catChoices)
     pub_date = models.DateField()
     author_name = models.CharField(max_length=50, default="")
     author_info = models.CharField(max_length=200, default="")
